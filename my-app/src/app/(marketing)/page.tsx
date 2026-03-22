eb68b3e4b809aaa45c7393d7fab9dc0a3922f304
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-22 13:57:59 +0530

    feat: initialize Next.js application with TypeScript and Tailwind CSS
    - Added package.json with dependencies for Next.js, React, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS configuration.
    - Added SVG assets to the public directory.
    - Implemented layout components for authentication, marketing, and about pages.
    - Created home and about pages with basic content.
    - Configured TypeScript settings in tsconfig.json.

---
+++ my-app/src/app/(marketing)/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>WELCOME TO THE HOME PAGE</h1>
+}
---
+++ my-app/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+// static metadata for the about page, which sets the title of the page to "About Us". This metadata can be used by Next.js for SEO purposes and to display the correct title in the browser tab when the user navigates to the about page.
+
+export const metadata = {
+    title: "About Us",
+}
+
+export default function About() {
+    return <h1>WELCOME TO THE ABOUT US PAGE</h1>
+}
