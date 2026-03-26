e675283e91a904a832a7652806955b389178cab9
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-26 22:03:43 +0530

    feat: Learning about route groups and private folders

---
+++ next-app/app/components/page.js
@@ -0,0 +1,15 @@
+// private folders
+
+import Test from "./test";
+
+export default function components() {
+    return (
+        
+        <div>
+            <Test />
+            <h1>
+                Welcome to Components Page
+            </h1>
+        </div>
+    )
+}
---
+++ next-app/app/components/test.js
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+        <div>
+            <h1>
+                Welcome to Test Page
+            </h1>
+        </div>
+    )
+}
